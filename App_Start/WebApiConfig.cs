ec40860ac8ef3cb7b957cc12230d2cfa6847db8e
Dheepa <Dheepa>
2020-05-15 00:36:23 +0530

    Enabled Cors functionality for Angualr project

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Http;
-
+using System.Web.Http.Cors;
 namespace WebApiTraining
 {
     public static class WebApiConfig
     {
+
         public static void Register(HttpConfiguration config)
         {
+            string angularuri = ConfigurationManager.AppSettings["AngularAppUri"];
             // Web API configuration and services
-
+            config.EnableCors(new EnableCorsAttribute(angularuri, headers: "*", methods: "*"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
